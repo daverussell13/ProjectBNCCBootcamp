a9ceb2f3a58f3d006b9ef60d6bfd65fad1dadf2f
Dave Russell <daverussell1309@gmail.com>
2022-09-04 20:55:11 +0700

    add admin guards

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,16 +6,21 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route("user.login");
-        }
+  /**
+   * Get the path the user should be redirected to when they are not authenticated.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return string|null
+   */
+  protected function redirectTo($request)
+  {
+    if (!$request->expectsJson()) {
+      if ($request->routeIs("admin.*")) {
+        return route("admin.login");
+      }
+      if ($request->routeIs("user.*")) {
+        return route("user.login");
+      }
     }
+  }
 }
